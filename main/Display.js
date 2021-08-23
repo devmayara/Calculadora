f9bb7ec9c429e0bc797980e9bbf87656e9124c7d
Mayara G. Silva <devmayara11@gmail.com>
2021-08-23 13:23:28 -0300

    add

---
+++ main/Display.js
@@ -0,0 +1,46 @@
+
+class Display {
+    constructor (displayValorAnterior, displayValorAtual) {
+        this.displayValorAtual = displayValorAtual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculadora = new Calculadora();
+        this.tipoOperacao = undefined;
+        this.valorAtual = '';
+        this.valorAnterior = '';
+    }
+
+    apagar() {
+        this.valorAtual = this.valorAtual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+
+    apagarTudo() {
+        this.valorAtual = '';
+        this.valorAnterior = '';
+        this.tipoOperacao = undefined;
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero) {
+        if(numero === '.' && this.valorAtual.includes('.')) return 
+        this.valorAtual = this.valorAtual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores() {
+        this.displayValorAtual.textContent = this.valorAtual;
+        this.displayValorAnterior.textContent = this.valorAnterior;
+    }
+
+    calcular() {
+        const valorAnterior = porsefloot(this.valorAnterior);
+        const valorAtual = porsefloot(this.valorAtual);
+
+        if ( isNaN(valorAtual) || isNaN(valorAnterior) ) return
+        this.valorAtual = this.calculadora[this.tipoOperacao](valorAnterior, valorAtual);
+    }
+
+    computar() {
+        
+    }
+}
